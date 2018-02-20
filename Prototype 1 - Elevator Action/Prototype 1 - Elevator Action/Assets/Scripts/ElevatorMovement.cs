cb81e5703c8cc2152d2d7c32b605a63b25d96d97
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2018-02-20 14:18:56 +0000

    Prototype 1
    unity files for future reference

---
+++ Prototype 1 - Elevator Action/Prototype 1 - Elevator Action/Assets/Scripts/ElevatorMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool canMove = true;
+    private bool Pause = true;
+
+    public Transform[] targetPositions;
+    private int target = 0;
+
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPositions[target].transform.position, Time.deltaTime * moveSpeed);
+        }
+        if (transform.position.y == targetPositions[target].position.y)
+        {
+            if (Pause)
+            {
+                StartCoroutine(Wait());
+                Pause = false;
+            }
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(2);
+        target++;
+        if (target == targetPositions.Length)
+        {
+            target = 0;
+        }
+        canMove = true;
+        Pause = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Enemy")
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Enemy")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
---
+++ Prototype 1 - Elevator Action/Prototype 1 - Elevator Action/Assets/Scripts/StairScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairScript : MonoBehaviour
+{
+    public Transform target;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public void StairsMove(GameObject other)
+    {
+        other.transform.position = target.transform.position;
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<EnemyMovement>().currentWaitTime = other.GetComponent<EnemyMovement>().maxWaitTime;
+            other.GetComponent<EnemyMovement>().mode = 0;
+        }
+    }
+}
